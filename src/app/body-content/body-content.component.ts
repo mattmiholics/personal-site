86aec4913277c8f404d4175e8ebcd9c131fe28e3
Matt Miholics <mmiholics8@gmail.com>
2022-10-25 11:35:52 -0400

    nerf this

--- src/app/body-content/body-content.component.ts
+++ src/app/body-content/body-content.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-body-content',
